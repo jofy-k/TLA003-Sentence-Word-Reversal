020bd3e696b1781fcdbbcefc67128878d7917555
jofy-k <kliboon_240000001117@uic.edu.ph>
2025-05-16 18:44:55 +0800

    Update Liboon_TLA003.java

--- libs/src/main/java/com/mycompany/libs/Liboon_TLA003.java
+++ libs/src/main/java/com/mycompany/libs/Liboon_TLA003.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.libs;
 
 import java.util.Scanner;
@@ -9,17 +8,26 @@ public class Liboon_TLA003 {
         Stack <String> stack = new Stack <> ();
         Scanner input = new Scanner (System.in);
         
-        System.out.println("Enter a word to reverse: ");
-        String sentence = input.nextLine();
-        
-        
-        String [] words = sentence.split(" ");
-        
-        for(String word : words){
-            stack.push(word);
+        while (true) {
+            System.out.println("Enter words to reverse (enter q to end sentence): ");
+            String sentence = input.nextLine().trim(); // gets user input
+
+            if (sentence.equalsIgnoreCase("q")) { //if q is input stops
+                break;
+            } else {
+                stack.push(sentence); // pushes input into stack
+            }
         }
-        while (!stack.isEmpty()){
-            System.out.print(stack.pop() + " ");
+        
+        if (!stack.isEmpty() && stack.size() > 1) { //making sure stack is not empty
+            for (String words : stack) { // each words in stack will be printed
+                System.out.print(words + " "); 
+            }
+            System.out.println("\nReversed: "); //prints the reversed word
+            while (!stack.isEmpty()) {
+                String popped = stack.pop(); 
+                System.out.print(popped + " "); 
+            }
         }
-    }    
+    }
 }
